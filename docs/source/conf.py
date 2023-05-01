b766aca3eae40c5a7810c05f24f2a76a402265a5
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2023-05-01 11:51:12 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ROS2 Tutorials'
+copyright = '2023, Murilo M. Marinho'
+author = 'Murilo M. Marinho'
 
 release = '0.1'
 version = '0.1.0'
