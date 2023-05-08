710afa7cf606bdcb02f167236e6703883e6761b1
Murilo M. Marinho <murilomarinho@ieee.org>
2023-05-08 17:56:51 +0900

    [sample_python_library/_sample_class.py] Moving the _ to the correct place

--- ros2_tutorial_workspace/src/python_package_with_a_library/python_package_with_a_library/sample_python_library/_sample_class.py
+++ ros2_tutorial_workspace/src/python_package_with_a_library/python_package_with_a_library/sample_python_library/_sample_class.py
@@ -27,11 +27,11 @@ class SampleClass:
     """A sample class to check how they can be imported by other ROS2 packages."""
 
     def __init__(self, name: str):
-        self.name_ = name
+        self._name = name
 
     def get_name(self) -> str:
         """
         Gets the name of this instance.
         :return: This name.
         """
-        return self.name_
+        return self._name
