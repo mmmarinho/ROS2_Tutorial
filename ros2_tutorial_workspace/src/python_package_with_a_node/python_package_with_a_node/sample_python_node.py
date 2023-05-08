0c208d21f6935ea23b4aea09c74cdf93b369be5b
Murilo M. Marinho <murilomarinho@ieee.org>
2023-05-08 11:33:57 +0900

    Adding tutorial code as a ros2 workspace

---
+++ ros2_tutorial_workspace/src/python_package_with_a_node/python_package_with_a_node/sample_python_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from python_package_with_a_node.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ ros2_tutorial_workspace/src/python_package_with_a_node/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'python_package_with_a_node'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='murilo',
+    maintainer_email='murilomarinho@ieee.org',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'sample_python_node = python_package_with_a_node.sample_python_node:main'
+        ],
+    },
+)
