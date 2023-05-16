3f7269395247c1e26262d7fb7010d5c6f2462f06
Murilo M. Marinho <murilomarinho@ieee.org>
2023-05-16 14:04:20 +0900

    [python_package_with_a_library/setup.py]

--- ros2_tutorial_workspace/src/python_package_with_a_library/setup.py
+++ ros2_tutorial_workspace/src/python_package_with_a_library/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = 'python_package_with_a_library'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
