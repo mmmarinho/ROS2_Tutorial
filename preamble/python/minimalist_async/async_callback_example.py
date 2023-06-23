9cc941dcc16b8987941b4e7ef092c7032199d09e
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2023-06-23 15:33:37 +0900

    Update async_callback_example.py

--- preamble/python/minimalist_async/async_callback_example.py
+++ preamble/python/minimalist_async/async_callback_example.py
@@ -23,7 +23,7 @@ SOFTWARE.
 """
 from functools import partial
 import asyncio
-from async_await_example import unlikely_to_return
+from .async_await_example import unlikely_to_return
 
 
 def handle_return_callback(tag: str, future: asyncio.Future) -> None:
