d616a812329d282d5786393e78ad8b020972c5d0
Murilo M. Marinho <murilomarinho@ieee.org>
2023-07-06 14:15:37 +0900

    [cpp_package_with_a_node] First draft

---
+++ ros2_tutorial_workspace/src/cpp_package_with_a_node/src/print_forever_cpp_node.cpp
@@ -0,0 +1,56 @@
+/*
+MIT LICENSE
+
+Copyright (C) 2023 Murilo Marques Marinho (www.murilomarinho.info)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#include "print_forever_cpp_node.hpp"
+
+#include <chrono>
+using namespace std::chrono_literals;
+
+/**
+ * @brief PrintForeverCPP::PrintForeverCPP.
+ * Default constructor giving default values to the node and members.
+ */
+PrintForeverCPP::PrintForeverCPP():
+    rclcpp::Node("print_forever_cpp"),
+    timer_period_(0.5),
+    print_count_(0)
+{
+    timer_ = create_wall_timer(
+                std::chrono::milliseconds(long(timer_period_*1e3)),
+                std::bind(&PrintForeverCPP::_timer_callback, this)
+                );
+}
+
+/**
+ * @brief PrintForeverCPP::_timer_callback.
+ * Periodically prints using RCLCPP_INFO.
+ */
+void PrintForeverCPP::_timer_callback()
+{
+    RCLCPP_INFO_STREAM(get_logger(),
+                       std::string("Printed ") +
+                       std::to_string(print_count_) +
+                       std::string(" times.")
+                       );
+    print_count_++;
+}
