621744fdfd2c9c0962cc242e86dc022aff7be068
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2025-07-28 01:39:13 +0000

    [python_package_that_uses_the_services] Switching from AmazingQuote to AddPoints.

--- ros2_tutorial_workspace/src/python_package_that_uses_the_services/python_package_that_uses_the_services/what_is_the_point_service_server_node.py
+++ ros2_tutorial_workspace/src/python_package_that_uses_the_services/python_package_that_uses_the_services/what_is_the_point_service_server_node.py
@@ -1,7 +1,7 @@
 """
 MIT LICENSE
 
-Copyright (C) 2023 Murilo Marques Marinho (www.murilomarinho.info)
+Copyright (C) 2023-25 Murilo Marques Marinho (www.murilomarinho.info)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,61 +26,33 @@ from textwrap import dedent  # https://docs.python.org/3/library/textwrap.html#t
 
 import rclpy
 from rclpy.node import Node
-from package_with_interfaces.srv import WhatIsThePoint
+from package_with_interfaces.srv import AddPoints
 
 
-class WhatIsThePointServiceServerNode(Node):
-    """A ROS2 Node with a Service Server for WhatIsThePoint."""
+class AddPointsServiceServerNode(Node):
+    """A ROS2 Node with a Service Server for AddPoints."""
 
     def __init__(self):
         super().__init__('what_is_the_point_service_server')
 
         self.service_server = self.create_service(
-            srv_type=WhatIsThePoint,
-            srv_name='/what_is_the_point',
-            callback=self.what_is_the_point_service_callback)
+            srv_type=AddPoints,
+            srv_name='/add_points',
+            callback=self.add_points_service_callback)
 
         self.service_server_call_count: int = 0
 
-    def what_is_the_point_service_callback(self,
-                                           request: WhatIsThePoint.Request,
-                                           response: WhatIsThePoint.Response
-                                           ) -> WhatIsThePoint.Response:
-        """Analyses an AmazingQuote and returns what is the point.
-           If the quote contains 'life', it returns a point whose sum of coordinates is 42.
-           Otherwise, it returns a random point whose sum of coordinates is not 42.
+    def add_points_service_callback(self,
+                                   request: AddPoints.Request,
+                                   response: AddPoints.Response
+                                   ) -> AddPoints.Response:
+        """
+        Adds the two points `a` and `b` in the request and returns the `result`.
         """
 
-        # Generate the x,y,z of the point
-        if "life" in request.quote.quote.lower():
-            x: float = random.uniform(0, 42)
-            y: float = random.uniform(0, 42 - x)
-            z: float = 42 - (x + y)
-        else:
-            x: float = random.uniform(0, 100)
-            y: float = random.uniform(0, 100)
-            z: float = random.uniform(0, 100)
-            if x + y + z == 42:  # So you’re telling me there’s a chance? Yes!
-                x = x + 1  # Not anymore :(
-
-        # Assign to the response
-        response.point.x = x
-        response.point.y = y
-        response.point.z = z
-
-        # Increase the call count
-        self.service_server_call_count = self.service_server_call_count + 1
-
-        self.get_logger().info(dedent(f"""
-            This is the call number {self.service_server_call_count} to this Service Server.
-            The analysis of the AmazingQuote below is complete.
-            
-                    {request.quote.quote}
-            
-            -- {request.quote.philosopher_name}
-            
-            The point has been sent back to the client.
-        """))
+        response.result.x = request.a.x + request.b.x
+        response.result.y = request.a.y + request.b.y
+        response.result.z = request.a.z + request.b.z
 
         return response
 
@@ -94,9 +66,9 @@ def main(args=None):
     try:
         rclpy.init(args=args)
 
-        what_is_the_point_service_server_node = WhatIsThePointServiceServerNode()
+        add_points_service_server_node = AddPointsServiceServerNode()
 
-        rclpy.spin(what_is_the_point_service_server_node)
+        rclpy.spin(add_points_service_server_node)
     except KeyboardInterrupt:
         pass
     except Exception as e:
