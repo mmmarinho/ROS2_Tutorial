41b328c978b9e99be16a44f76e88b04cd5caf0da
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2025-10-13 20:40:23 +0100

    [postCreateCommand.sh] Adding tree install.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -8,10 +8,10 @@ pip install -r docs/source/requirements.txt
 pip install sphinx-autobuild
 chmod +x .devcontainer/serve_sphinx.sh
 
-# Adding other useful packages
-sudo apt-get install tree
-
 # Making mermaid graphs work
 sudo apt-get update
 sudo apt-get install -y npm
 npm install -g @mermaid-js/mermaid-cli
+
+# Adding other useful packages
+sudo apt-get install -y tree
