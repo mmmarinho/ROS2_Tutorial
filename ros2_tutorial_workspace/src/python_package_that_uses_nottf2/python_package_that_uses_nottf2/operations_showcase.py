fa4afdf2aaa825b7a7ddca4142625207d038a17c
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2025-11-02 13:13:02 +0000

    [operations_showcase] Working.

--- ros2_tutorial_workspace/src/python_package_that_uses_nottf2/python_package_that_uses_nottf2/operations_showcase.py
+++ ros2_tutorial_workspace/src/python_package_that_uses_nottf2/python_package_that_uses_nottf2/operations_showcase.py
@@ -1,12 +1,21 @@
 from math import pi
+from geometry_msgs.msg import Quaternion
 import marinholab.nottf2 as ntf2
 
 def main():
-    r1 = ntf2.rotx(phi=pi/2)
-    r2 = ntf2.rotz(phi=pi/2)
-    r12 = ntf2.quaternion_multiply(r1, r2)
+    phi: float = pi / 2.0
 
-    print(f"The quaternion multiplication of {r1} and {r2} is {r12}.")
+    r1: Quaternion = ntf2.rotx(phi=phi)
+
+    print(f"The rotation of {phi} radians about the x-axis is {r1}.")
+
+    r2: Quaternion = ntf2.rotz(phi=phi)
+
+    print(f"The rotation of {phi} radians about the z-axis is {r1}.")
+
+    r12: Quaternion = ntf2.quaternion_multiply(r1, r2)
+
+    print(f"The quaternion multiplication of r12=r1r2 is {r12}.")
 
 if __name__ == '__main__':
     main()
