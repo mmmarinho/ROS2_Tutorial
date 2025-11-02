4930203ea69f5fe2a95cb6ad7f05c4605a8de738
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2025-11-02 13:17:05 +0000

    Adding rotation inverse.

--- ros2_tutorial_workspace/src/python_package_that_uses_nottf2/python_package_that_uses_nottf2/operations_showcase.py
+++ ros2_tutorial_workspace/src/python_package_that_uses_nottf2/python_package_that_uses_nottf2/operations_showcase.py
@@ -7,15 +7,19 @@ def main():
 
     r1: Quaternion = ntf2.rotx(phi=phi)
 
-    print(f"The rotation of {phi} radians about the x-axis is {r1}.")
+    print(f"The rotation of {phi} radians about the x-axis is r1={r1}.")
+
+    r1_conj : Quaternion = ntf2.rotation_inverse(r1)
+
+    print(f"The inverse rotation of r1 is r1_conj={r1_conj}.")
 
     r2: Quaternion = ntf2.rotz(phi=phi)
 
-    print(f"The rotation of {phi} radians about the z-axis is {r1}.")
+    print(f"The rotation of {phi} radians about the z-axis is r2={r2}.")
 
     r12: Quaternion = ntf2.quaternion_multiply(r1, r2)
 
-    print(f"The quaternion multiplication of r12=r1r2 is {r12}.")
+    print(f"The quaternion multiplication of r12=r1r2 is r12={r12}.")
 
 if __name__ == '__main__':
     main()
